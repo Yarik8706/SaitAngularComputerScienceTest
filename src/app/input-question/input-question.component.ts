efd4112504d6171f5bef2df7a86002005d145b4e
Yarik8706 <yarikno08@gmail.com>
2021-12-23 22:26:34 +0300

    2 files modified

--- src/app/input-question/input-question.component.ts
+++ src/app/input-question/input-question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ControlAnswerService } from '../control-answer.service';
 
 @Component({
   selector: 'app-input-question',
@@ -7,9 +8,26 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class InputQuestionComponent implements OnInit {
   @Input() title: string;
-  @Input() trueAnswer: string;
+  @Input() stringTrueAnswers: string;
+  public trueAnswers: string;
 
-  constructor() {}
+  constructor(private controlAnswerService: ControlAnswerService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.trueAnswers = JSON.parse(this.stringTrueAnswers);
+  }
+
+  setUserAnswer(value: string): void {
+    let count = 0;
+    for (let trueAnswer of this.trueAnswers) {
+      if (value.includes(trueAnswer)) {
+        count++;
+      }
+    }
+    if (count == 3) {
+      this.controlAnswerService.AddUserAnswer(this.title, true);
+    } else {
+      this.controlAnswerService.AddUserAnswer(this.title, false);
+    }
+  }
 }
